12cb8b11265125861ea66b7001318534a4ffd7a7
John Radojevich <johnathanradojevich@gmail.com>
2022-06-04 18:29:04 -0500

    Added basic styling.

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,4 +2,15 @@
 	import '../app.css';
 </script>
 
+<nav class="fixed right-0 mt-4 mr-12 flex space-x-6 text-xl text-white">
+	<a>Projects</a>
+	<a>Blog</a>
+	<a href="./static/Resume.pdf" download>Resume</a>
+</nav>
+
+<span class='fixed bottom-10 animate-bounce bg-white dark:bg-slate-800 p-2 w-10 h-10 ring-1 ring-slate-900/5 dark:ring-slate-200/20 shadow-lg rounded-full inline-flex items-center justify-center'>
+	<svg fill='none' stroke-linecap='round' stroke-linejoin='round' stroke-width='2' viewBox='0 0 24 24' stroke='currentColor' width='32px' height='32px' class='mx-auto text-red-500'>
+		<path d="M19 14l-7 7m0 0l-7-7m7 7V3"></path>
+	</svg>
+</span>
 <slot />
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,18 +1,32 @@
-<main class='snap-y snap-mandatory overflow-y-scroll h-screen'>
-	<section class='snap-center h-full bg-[#120930] grid grid-cols-2'>
-		<h1 class='text-6xl text-white font-bold ml-20 mt-40 transition-opacity'>Hello,<br>I'm John Radojevich</h1>
-		<div class='flex'>
-			<img src={globe} class='w-80 h-80 m-auto'/>
+<script>
+	import globe from '../dark2.svg';
+</script>
+
+<svelte:head>
+	<title>John Radojevich: Full Stack Web Developer, GIS / Data Visualization Specialist</title>
+</svelte:head>
+
+<main class="h-screen snap-y snap-mandatory overflow-y-scroll">
+	<section
+		class="grid h-full snap-center bg-[#120930] from-zinc-800 to-zinc-900 dark:bg-gradient-to-b lg:grid-cols-2 md:grid-cols-1"
+	>
+		<div class="flex flex-col justify-center align-middle">
+			<h1 class="text-right text-7xl text-white">Hello;<br />I'm <span class='font-bold'> John Radojevich</span></h1>
+			<p class="mt-6 bg-zinc-900 p-4 text-right text-gray-300 shadow-sm shadow-red-500">
+				To me, maps are so much more than diagrams that help us navigate from point A to point B.
+				<br>
+				They are windows into information that allow us to explore our world, understand it better, and
+				engage with it more productively. They spark our curiosity, provide insight, and help us
+				build solutions for a better world.
+			</p>
+		</div>
+		<div class="flex">
+			<img src={globe} class="m-auto h-100 w-100" />
 		</div>
 	</section>
-	<section class='snap-center h-full'>
-		<h1>Insert Section 2</h1>
-	</section>
-	<section class='snap-center h-full'>
-		<h1>Insert Section 3</h1>
+	<section class="h-full snap-center dark:bg-zinc-900">
+			<h1 class="text-white text-7xl pt-10">Experienced.</h1>
+			<h2 class='text-white text-3xl mt-5'>To me, development is more than a career.</h2>
 	</section>
 </main>
 
-<script>
-	import globe from '../earth.svg'
-</script>
