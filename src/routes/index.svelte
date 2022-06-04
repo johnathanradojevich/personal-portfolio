a0d01ae8a6b30dfbb5ec23fff50da352f1ef66ba
John Radojevich <johnathanradojevich@gmail.com>
2022-06-04 13:35:38 -0500

    Added Earth SVG.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,3 +1,18 @@
-<h1 class='text-3xl font-bold underline'>
-	Hello World!
-</h1>
+<main class='snap-y snap-mandatory overflow-y-scroll h-screen'>
+	<section class='snap-center h-full bg-[#120930] grid grid-cols-2'>
+		<h1 class='text-6xl text-white font-bold ml-20 mt-40 transition-opacity'>Hello,<br>I'm John Radojevich</h1>
+		<div class='flex'>
+			<img src={globe} class='w-80 h-80 m-auto'/>
+		</div>
+	</section>
+	<section class='snap-center h-full'>
+		<h1>Insert Section 2</h1>
+	</section>
+	<section class='snap-center h-full'>
+		<h1>Insert Section 3</h1>
+	</section>
+</main>
+
+<script>
+	import globe from '../earth.svg'
+</script>
