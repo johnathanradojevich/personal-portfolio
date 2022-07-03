3c634ce614056981e7613e41c5c9be1f99352b2b
John Radojevich <johnathanradojevich@gmail.com>
2022-07-02 22:48:43 -0500

    updated layout and styling to a basic black site.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,3 +1,36 @@
-<h1 class='text-3xl font-bold underline'>
-	Hello World!
-</h1>
+<div class='grid grid-cols-1 h-screen bg-black text-white'>
+	<div class='place-self-center'>
+		<h1 id='name' class='text-6xl font-bold font-Gemunu tracking-widest'></h1>
+		<h2 class='font-Gemunu text-2xl'>Hey! 😎 I'm a Full-Stack Software Developer @ Terracon 🌎</h2>
+		<div class='mt-10'>
+			<a href='./static/resume.pdf' class='border-2 p-2 pt-1.5'>Resume</a>
+			<a href='https://github.com/johnathanradojevich' class='border-2 p-2 pt-1.5'>GitHub</a>
+			<a href='https://www.linkedin.com/in/john-radojevich' class='border-2 p-2 pt-1.5'>LinkedIn</a>
+			<a href='mailto:johnathanradojevich@gmail.com' class='border-2 p-2 pt-1.5'>Email</a>
+			<a href='https://blog.johnradojevich.dev' class='border-2 p-2 pt-1.5'>Blog</a>
+		</div>
+	</div>
+</div>
+
+<script>
+
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+
+		const text = 'John Radojević';
+		const header = document.getElementById('name');
+
+		header.textContent = text[0];
+		let typingIdx = 0;
+
+		const interval = setInterval(() => {
+			++typingIdx;
+			header.textContent = text.slice(0, typingIdx);
+			if (typingIdx >= text.length) {
+				clearInterval(interval);
+			}
+		}, 60);
+	});
+
+</script>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,11 @@
 module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
-  },
-  plugins: [],
+    extend: {
+      fontFamily: {
+        Gemunu: ['Gemunu Libre', 'sans-serif'],
+      },
+    },
+    plugins: [],
+  }
 }
